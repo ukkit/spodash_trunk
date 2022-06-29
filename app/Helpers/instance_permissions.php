3d2be8104a50aa37cc1f5d317dd3cbc5f5368aa9
Neeraj Tikku <neeraj.tikku@gmail.com>
2022-06-29 18:22:56 +0530

    first set of changed

---
+++ app/Helpers/instance_permissions.php
@@ -0,0 +1,7 @@
+<?php
+
+function have_team_permissions($instance_details_id)
+{
+    // Logic, based on intellicus_details_id, look for instance_details_id in team_permissions table
+    // If found, return true
+}
