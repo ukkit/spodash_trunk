e32ebedf4c782f67b3451b97008118f6eba7757d
Neeraj Tikku <neeraj.tikku@gmail.com>
2022-07-15 11:21:14 +0530

    checking permission to add ms details

--- resources/views/ml_details/index.blade.php
+++ resources/views/ml_details/index.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+
     <section class="content-header">
         <h1 class="pull-left">Machine Learning Details</h1>
         <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{{ route('mlDetails.create') }}">Add New</a>
+            @can('add_databaseDetails')
+                <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{{ route('mlDetails.create') }}">Add New</a>
+            @endcan
         </h1>
     </section>
     <div class="content">
