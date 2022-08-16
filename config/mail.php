c7dd9f2c1b06a72cf1762f7dddf4b11ff687a6ea
Shift <shift@laravelshift.com>
2022-08-16 04:21:47 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them and merged your true customizations -
    where ENV variables may not be used.

--- config/mail.php
+++ config/mail.php
@@ -11,8 +11,8 @@ return [
     | sending of e-mail. You may specify which one you're using throughout
     | your application here. By default, Laravel is setup for SMTP mail.
     |
-    | Supported: "smtp", "sendmail", "mailgun", "mandrill", "ses",
-    |            "sparkpost", "log", "array"
+    | Supported: "smtp", "sendmail", "mailgun", "ses",
+    |            "postmark", "log", "array"
     |
     */
 
@@ -56,11 +56,10 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'slmqauser@ptc.com'),
-        'name' => env('MAIL_FROM_NAME', 'SPO-Dashboard'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
-    // 'from' => ['address' => 'donotreply@ptc.com', 'name' => 'SPODash-Mail'],
     'reply_to' => ['address' => 'spo-incredibles@ptc.com', 'name' => 'SPO-Incredibles'],
 
     /*
